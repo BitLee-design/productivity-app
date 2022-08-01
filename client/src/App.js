3fa95d6fbb6342feb103c79278afb4502e2a727b
BitLee-design <71900108+BitLee-design@users.noreply.github.com>
2022-08-01 23:38:00 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 const App = () => {
@@ -16,22 +17,6 @@ const App = () => {
     };
     fetchData();
   }, []);
-
-  <main className="app-main">
-    <h2>Today</h2>
-
-  {activities && activities.length > 0 ? (
-    <ol>
-      {activities.map((activity) => (
-        <li key={activity._id}>
-          {activity.name} - {activity.time}
-        </li>
-      ))}
-    </ol>
-  ) : (
-    <p>No activities yet</p>
-  )}
-  </main>
 
   /* New constant to connect the front to back */
   const cors = require("cors");
@@ -63,7 +48,7 @@ const App = () => {
       window.location.reload(); // reloads the window after sending request
       };
   
-      <form onSubmit={addActivity}></form>
+      <form onSubmit={addActivity}></form>  
   }
 
   return (
@@ -87,15 +72,23 @@ const App = () => {
           <button type="submit">Add</button>
         </form>
       </header>
+    
       <main className="app-main">
         <h2>Today</h2>
-        <ol>
-            <li> Activity 1 - 3 hours</li>
-            <li> Activity 2 - 10 min</li>
-            <li> Activity 3 - 2 hours and 25 min</li>
-        </ol>
-      </main>
-    </div>
+
+        {activities && activities.length > 0 ? (
+          <ol>
+            {activities.map((activity) => (
+              <li key={activity._id}>
+                {activity.name} - {activity.time}
+              </li>
+            ))}
+          </ol>
+        ) : (
+          <p>No activities yet</p>
+        )}
+    </main>  
+   </div>
   );
 }
 
