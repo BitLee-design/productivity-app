fec4b85537181acb7ae191e88e64c1e90e63b26c
BitLee-design <71900108+BitLee-design@users.noreply.github.com>
2022-08-03 03:55:17 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,32 +25,28 @@ const App = () => {
   App.use(cors());
 
   /* New function to create a submit element */
-  function handleSubmit() {
 
-    const addActivity = async (event) => {
-      event.preventDefault();
+  const addActivity = async (event) => {
+    event.preventDefault();
       
-      const newActivity = {
-        name: event.target.activity.value,
-        time: event.target.time.value,
-      };
+    const newActivity = {
+      name: event.target.activity.value,
+      time: event.target.time.value,
+    };
       
-      await fetch(`${process.env.REACT_APP_BACKEND_URL}/activity`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newActivity),
-      });
+    await fetch(`${process.env.REACT_APP_BACKEND_URL}/activity`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newActivity),
+    });
       
-      event.target.activity.value = ""; // sets input empty after clicking submit
-      event.target.time.value = ""; // sets input empty after clicking submit
-      window.location.reload(); // reloads the window after sending request
-      };
+    event.target.activity.value = ""; // sets input empty after clicking submit
+    event.target.time.value = ""; // sets input empty after clicking submit
+    window.location.reload(); // reloads the window after sending request
+  };
   
-      <form onSubmit={addActivity}></form>  
-  }
-
   return (
     <div className="app">
       <header className="app-header">
